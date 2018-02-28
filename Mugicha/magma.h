36d590e4069baf2431e0bf6ab214905cda46e963
lm9 <depkey@me.com>
2018-02-28 23:20:04 +0900

    Magma::get_square()は不要

--- Mugicha/magma.h
+++ Mugicha/magma.h
@@ -9,6 +9,5 @@ class Magma : public ScalableObject
 {
 public:
 	Magma(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, D3DXVECTOR2 &_camera, float _u = 0.0f, float _v = 0.0f, float _uw = 1.0f, float _vh = 1.0);
-	SQUARE get_square();
 private:
 };
