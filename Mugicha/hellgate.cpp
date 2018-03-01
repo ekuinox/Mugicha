0de3f08037c475c5ec43a1fbb54b342f86f5b504
lm9 <depkey@me.com>
2018-03-02 01:04:45 +0900

    HellGate, 下からグイグイ上がってくるヤツ．追加はしたけど導入はまだ

---
+++ Mugicha/hellgate.cpp
@@ -0,0 +1,20 @@
+#include "hellgate.h"
+
+HellGate::HellGate(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, D3DXVECTOR2 & _camera, float _u, float _v, float _uw, float _vh)
+	: ScalableObject(_x, _y, _w, _h, _tex, _layer, _camera, _u, _v, _uw, _vh)
+{
+	speed = 0.01f;
+}
+
+void HellGate::update()
+{
+	unless(status) return;
+
+	auto current = std::chrono::system_clock::now();
+	if (std::chrono::duration_cast<std::chrono::milliseconds>(current - latest_update).count() > UPDATE_INTERVAL)
+	{
+		y += speed;
+
+		latest_update = current;
+	}
+}
---
+++ Mugicha/hellgate.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "scalable_object.h"
+class HellGate : public ScalableObject
+{
+public:
+	HellGate(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, D3DXVECTOR2 &_camera, float _u = 0.0f, float _v = 0.0f, float _uw = 1.0f, float _vh = 1.0f);
+	void update();
+private:
+};
+
