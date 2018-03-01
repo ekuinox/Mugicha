e6cdc3d1075b553eea0cd1d0b66e926303f559e9
lm9 <depkey@me.com>
2018-03-01 21:55:47 +0900

    関数定義

--- Mugicha/always_displayed_polygon.h
+++ Mugicha/always_displayed_polygon.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "plain_square_polygon.h"
+#include "square_polygon_base.h"
 
 /*
 * AlwaysDisplayedPolygon
@@ -7,10 +7,38 @@
 *
 */
 
-class AlwaysDisplayedPolygon : public PlainSquarePolygon
+class AlwaysDisplayedPolygon : public SquarePolygonBase
 {
+public:
+	AlwaysDisplayedPolygon(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, float _u = 0.0f, float _v = 0.0f, float _uw = 1.0f, float _vh = 1.0f);
+	~AlwaysDisplayedPolygon();
+
+	void init();
+	void update();
+	void draw();
+	bool is_drawing();
+	void show();
+	void hide();
+	bool is_active();
+	void enable();
+	void disable();
+
+	void on();
+	void off();
+
+	void change_texture(LPDIRECT3DTEXTURE9 _tex);
+
+	SQUARE get_square();
+	D3DXVECTOR2 get_coords();
+	POLSIZE get_size();
+	VERTEX_2D *get_vertexes();
+
+	void add_coord(float _x, float _y);
+
+	void zoom(POLSIZE _zoom_level);
+
+	AlwaysDisplayedPolygon* operator->();
 protected:
 	void generate_vertexes();
-public:
 };
 
