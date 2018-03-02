ea41cd30070c0a25a80a6422b71ea2cab4859805
lm9 <depkey@me.com>
2018-03-02 23:57:59 +0900

    変数名変更

--- Mugicha/thorn.h
+++ Mugicha/thorn.h
@@ -21,10 +21,10 @@ private:
 	bool falling;
 	std::map<SquarePolygonBase::PolygonTypes, std::vector<SquarePolygonBase*>> &polygons;
 	std::chrono::system_clock::time_point stop_time;
-	const long reloading_time; // Ä‘•“UŠÔ
+	const long interval; // Ä‘•“UŠÔ
 public:
 	Thorn(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, D3DXVECTOR2 &_camera, enum Vec _vec, std::map<SquarePolygonBase::PolygonTypes, std::vector<SquarePolygonBase*>> &_polygons, float _u = 0.0f, float _v = 0.0f, float _uw = 1.0f, float _vh = 1.0f);
-	Thorn(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, D3DXVECTOR2 &_camera, enum Vec _vec, std::map<SquarePolygonBase::PolygonTypes, std::vector<SquarePolygonBase*>> &_polygons, bool _attack, long _reloading_time = 5000, float _u = 0.0f, float _v = 0.0f, float _uw = 1.0f, float _vh = 1.0f);
+	Thorn(float _x, float _y, float _w, float _h, LPDIRECT3DTEXTURE9 _tex, int _layer, D3DXVECTOR2 &_camera, enum Vec _vec, std::map<SquarePolygonBase::PolygonTypes, std::vector<SquarePolygonBase*>> &_polygons, bool _attack, long _interval = 5000, float _u = 0.0f, float _v = 0.0f, float _uw = 1.0f, float _vh = 1.0f);
 	void set_floor(std::vector<SquarePolygonBase*> _floors); // floor‚ªnullptr‚Ì‚Ü‚Ü‚Ìê‡‚±‚±‚Åİ’è‚µ‚È‚¨‚µ‚Ü‚·D
 	void set_floor(SquarePolygonBase *_floor); // floor‚ªnullptr‚Ì‚Ü‚Ü‚Ìê‡‚±‚±‚Åİ’è‚µ‚È‚¨‚µ‚Ü‚·D
 	void init();
