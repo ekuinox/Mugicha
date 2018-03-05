c184be2707687e0154521a692159ade630d97965
lm9 <depkey@me.com>
2018-03-05 16:06:10 +0900

    ジャンプ力など

--- Mugicha/keyconf.h
+++ Mugicha/keyconf.h
@@ -9,10 +9,10 @@
 #define PLAYER_MOVE_RIGHT (GetKeyboardPress(DIK_D) || GetControllerButtonPress(XIP_D_RIGHT)) // 右移動
 #define PLAYER_JUMP (GetKeyboardTrigger(DIK_SPACE) || GetControllerButtonTrigger(XIP_A)) // ジャンプ
 #define PLAYER_JUMP_HOLD (GetKeyboardPress(DIK_SPACE) || GetControllerButtonPress(XIP_A)) // ジャンプ
-#define PLAYER_ITEM_USE (GetKeyboardPress(DIK_I) || GetControllerButtonPress(XIP_RB)) //  アイテムを掴んだり離したり
+#define PLAYER_ITEM_USE (GetKeyboardPress(DIK_I) || GetControllerButtonPress(XIP_B)) //  アイテムを掴んだり離したり
 
-#define ZOOM_MIN (GetKeyboardTrigger(DIK_L) || GetControllerButtonTrigger(XIP_B)) // 最小
-#define ZOOM_DEF (GetKeyboardTrigger(DIK_K) || GetControllerButtonTrigger(XIP_Y)) // 普通
-#define ZOOM_MAX (GetKeyboardTrigger(DIK_J) || GetControllerButtonTrigger(XIP_X)) // 最大
+#define ZOOM_MIN (GetKeyboardTrigger(DIK_L) || GetControllerButtonTrigger(XIP_LB)) // 最小
+#define ZOOM_DEF (GetKeyboardTrigger(DIK_K) || GetControllerButtonTrigger(XIP_X)) // 普通
+#define ZOOM_MAX (GetKeyboardTrigger(DIK_J) || GetControllerButtonTrigger(XIP_RB)) // 最大
 
 #define KEY_BACK_TO_TITLE (GetKeyboardTrigger(DIK_F5) || GetControllerButtonTrigger(XIP_START)) // タイトルに戻るためのキー
--- Mugicha/player.h
+++ Mugicha/player.h
@@ -13,8 +13,8 @@
 // 初期値など
 #define PLAYER_SPEED (0.675f) // プレイや移動速度
 #define PLAYER_JUMP_POWER (2.0f) // ジャンプ力，1msにどんだけ飛ぶか
-#define PLAYER_JUMP_HEIGHT (CELL_HEIGHT * 2.5) // ジャンプする高さ
-#define PLAYER_HOLD_JUMP_HEIGHT (CELL_HEIGHT * 4.0) // 長押しジャンプする高さ
+#define PLAYER_JUMP_HEIGHT (CELL_HEIGHT * 0.6) // ジャンプする高さ
+#define PLAYER_HOLD_JUMP_HEIGHT (CELL_HEIGHT * 3.2) // 長押しジャンプする高さ
 #define PLAYER_FALLING (1.0f) // 落下速度
 #define PLAYER_WIDTH (CELL_WIDTH * 0.8) // プレイヤの幅
 #define PLAYER_HEIGHT (CELL_HEIGHT * 0.8) // プレイヤの高さ
